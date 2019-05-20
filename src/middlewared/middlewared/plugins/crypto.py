67dfbd00960b00b666bb751b4b131889b26b141d
Waqar Ahmed <waqarsonic1@gmail.com>
2019-05-20 16:22:39 +0500

    Generate CRL
    This commit adds a method to cryptokey service which enables us to generate a CRL for a list of certs using provided ca. There's a tricky situation here as to what happens if the root CA is compromised ? In normal world scenarios, that CA is removed from app's trust store and any subsequent certs it had issues wouldn't be validated by the app then. Making a CRL for a revoked root CA in normal cases doesn't make sense as the thief can sign a counter CRL saying that everything is fine. As our environment is controlled, i think we are safe to create a crl for root CA as well which we can publish for services which make use of it i.e openvpn and they'll know that the certs/ca's have been compromised.

--- src/middlewared/middlewared/plugins/crypto.py
+++ src/middlewared/middlewared/plugins/crypto.py
@@ -816,6 +816,68 @@ class CryptoKeyService(Service):
                 encryption_algorithm=serialization.NoEncryption()
             ).decode()
 
+    def generate_crl(self, ca, certs, next_update=1):
+        # There is a tricky case here - what happens if the root CA is compromised ?
+        # In normal world scenarios, that CA is removed from app's trust store and any
+        # subsequent certs it had issues wouldn't be validated by the app then. Making a CRL
+        # for a revoked root CA in normal cases doesn't make sense as the thief can sign a
+        # counter CRL saying that everything is fine. As our environment is controlled,
+        # i think we are safe to create a crl for root CA as well which we can publish for
+        # services which make use of it i.e openvpn and they'll know that the certs/ca's have been
+        # compromised.
+        #
+        # `ca` is either root ca which has been compromised as well or the parent of a ca
+        # which has been revoked. `certs` is a list of all certs ca inclusive which are to
+        # included in the CRL ( if root ca is compromised, it will be in `certs` as well ).
+        # TODO: Verify how openvpn handles root ca revocation if it gets included in CRL
+        # does it fail to start etc ?
+        private_key = self.load_private_key(
+            ca['privatekey']
+        )
+        ca_cert = x509.load_pem_x509_certificate(ca['certificate'].encode(), default_backend())
+
+        if not private_key:
+            return None
+
+        crl_builder = x509.CertificateRevocationListBuilder().issuer_name(x509.Name([
+            x509.NameAttribute(NameOID.COMMON_NAME, ca['common'])
+        ])).last_update(
+            datetime.datetime.utcnow()
+        ).next_update(
+            datetime.datetime.utcnow() + datetime.timedelta(next_update, 300, 0)
+        )
+
+        for cert in certs:
+            crl_builder.add_revoked_certificate(
+                x509.RevokedCertificateBuilder().serial_number(
+                    self.load_certificate(cert['certificate'])['serial']
+                ).revocation_date(
+                    cert['revoked_date']
+                ).build(
+                    default_backend()
+                )
+            )
+
+        # https://www.ietf.org/rfc/rfc5280.txt
+        # We should add AuthorityKeyIdentifier and CRLNumber at the very least
+
+        crl = crl_builder.add_extension(
+            x509.AuthorityKeyIdentifier(
+                x509.SubjectKeyIdentifier.from_public_key(
+                    ca_cert.public_key()
+                ).digest, None, None
+            ), False
+        ).add_extension(
+            x509.CRLNumber(1), False
+        ).sign(
+            private_key=private_key, algorithm=hashes.SHA256(),
+            backend=default_backend()
+        )
+
+        return crl.public_bytes(
+            serialization.Encoding.PEM
+        ).decode()
+
 
 class CertificateService(CRUDService):
 
