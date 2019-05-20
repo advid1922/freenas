edc93baf440f2038ca97d24f8810d39c404c6c9e
Waqar Ahmed <waqarsonic1@gmail.com>
2019-05-20 14:47:05 +0500

    Migrations for revoked field
    This commit adds migrations for fields which will help us revoke certificates and generate Certificate Revocation Lists.

--- gui/system/models.py
+++ gui/system/models.py
@@ -625,6 +625,14 @@ class CertificateBase(Model):
         verbose_name=_("Signing Certificate Authority"),
         on_delete=models.CASCADE
     )
+    cert_revoked = models.BooleanField(
+        verbose_name=_('Revoked Certificate'),
+        default=False
+    )
+    cert_revoked_date = models.DateTimeField(
+        verbose_name=_('Revoked Date'),
+        null=True
+    )
 
     def __str__(self):
         return self.cert_name
